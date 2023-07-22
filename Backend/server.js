fcc7b5dba0a862e98024132b4abe88225176e6a0
Ajithkumar M <Ajithkumar.m@expeditors.com>
2023-07-22 22:28:33 +0530

    basic backend setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ string: "Hello" });
+});
+
+app.listen(port, () => {
+  console.log("Blalalla");
+});
