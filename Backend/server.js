9203f82575123e3c1c9c50543e1669eb191143b9
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2023-07-23 18:03:17 +0530

    added middleware , router

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const apiRouter = require("./public/router/apiRouter");
 
-app.get("/api", (req, res) => {
-  res.status(200).json({ string: "Hello" });
+const port = process.env.PORT || 3000;
+
+// parse application/json
+app.use(express.json());
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 });
+
+app.use(apiLimiter);
+
+app.use("/api", apiRouter);
+
 
 app.listen(port, () => {
   console.log("Started");
