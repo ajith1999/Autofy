6a6bff04e259c58105b8fc32592f97d470076275
Ajithkumar M <Ajithkumar.m@expeditors.com>
2023-07-24 13:05:28 +0530

    Added Dialog Box

---
+++ frontend/src/app/datatype-dialog/datatype-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datatype-dialog',
+  templateUrl: './datatype-dialog.component.html',
+  styleUrls: ['./datatype-dialog.component.css']
+})
+export class DatatypeDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/datatypes/data.ts
@@ -0,0 +1,32 @@
+export interface DataType {
+  title: string;
+  description: string;
+  category: string;
+}
+
+export const datas: DataType[] = [
+  {
+    title: 'Custom Field',
+    description:
+      'Add custom data that will be generated sequentially or randomly',
+    category: 'Logistics',
+  },
+  {
+    title: 'Custom Field',
+    description:
+      'Add custom data that will be generated sequentially or randomly',
+    category: 'Logistics',
+  },
+  {
+    title: 'Custom Field',
+    description:
+      'Add custom data that will be generated sequentially or randomly',
+    category: 'Logistics',
+  },
+  {
+    title: 'Custom Field',
+    description:
+      'Add custom data that will be generated sequentially or randomly',
+    category: 'Logistics',
+  },
+];
